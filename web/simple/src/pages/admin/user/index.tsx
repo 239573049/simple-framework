6da91db3a7e79f3fcb7b82e0c6c9e79801ab1bdb
token <239573049@qq.com>
2022-11-03 23:53:49 +0800

    增加用户列表查询

--- web/simple/src/pages/admin/user/index.tsx
+++ web/simple/src/pages/admin/user/index.tsx
@@ -1,15 +1,47 @@
 import { Component, ReactNode } from "react";
-import UserLayout from "../../../components/admin/user";
+import userinfoapi from "../../../apis/userinfoapi";
+import { GetUserInfoInput } from "../../../modules/userInfo/GetUserInfoInput";
+import InputUserInfo from "./InputUserInfo";
+import TabUserInfo from "./TabUserInfo";
 
-
-
-class User extends Component {
-
-
-
-    render(): ReactNode {
-        return (<UserLayout />)
-    }
+interface IState {
+    input: GetUserInfoInput
 }
 
-export default User;
+interface IProps {
+
+}
+
+class UserLayout extends Component<IProps, IState>{
+    state: Readonly<IState> = {
+        input: {
+            keywords: undefined,
+            startTime: null,
+            endTime: null,
+            page: 1,
+            pageSize: 20
+        }
+    }
+    render(): ReactNode {
+        var { input } = this.state
+        return (
+            <div>
+                <InputUserInfo
+                    keywords={input.keywords}
+                    startTime={input.startTime}
+                    keywordsChange={(value: any) => console.log(value)}
+                    timeChange={(value: any) => {
+                        input.keywords = value
+                        console.log(input);
+
+                        this.setState({
+                            input
+                        })
+                    }}
+                    endTime={input.endTime} />
+                <TabUserInfo input={null} />
+            </div>
+        )
+    }
+}
+export default UserLayout
