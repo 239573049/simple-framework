a4a8fa4518455496bd524de375bfda11ab07299c
token <239573049@qq.com>
2022-12-09 01:02:57 +0800

    优化代码结构

--- src/module/Simple.AuditLog/Handles/AuditLogHandle.cs
+++ src/module/Simple.AuditLog/Handles/AuditLogHandle.cs
@@ -1,10 +1,18 @@
-﻿using Token.Dependency;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Token.Dependency;
 using Token.Handlers;
 
 namespace Simple.AuditLog.Handles;
 
 public class AuditLogHandle : ILoadEventHandler<SimpleAuditLogEto>, ITransientDependency
 {
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public AuditLogHandle(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
     public async Task HandleEventAsync(SimpleAuditLogEto eventData)
     {
         await Task.CompletedTask;
